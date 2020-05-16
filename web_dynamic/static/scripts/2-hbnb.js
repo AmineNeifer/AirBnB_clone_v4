74d261ef10bd5918b754e851e0b483044d980e00
AmineNeifer <amineneifer2000@gmail.com>
2020-05-17 00:37:47 +0100

    update 3-header.css to make style on #api_status.availabe work / 2-hbnb.js added code block that changes color of circle based on the availability of database, and status of data

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -21,4 +21,12 @@ $(document).ready(function () {
       $('.amenities h4').html(str);
     }
   });
+  const url = 'http://0.0.0.0:5001/api/v1/status/';
+  $.getJSON(url, function (data) {
+  if (data.status === 'OK') {
+  $('#api_status').addClass('available');
+  } else {
+    $('#api_status').removeClass('available');
+  }
+  });
 });
