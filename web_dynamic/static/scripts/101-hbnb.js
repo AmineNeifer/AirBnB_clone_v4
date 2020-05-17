cd0cda24894934c0a703b458402eb49e68e53098
AmineNeifer <amineneifer2000@gmail.com>
2020-05-17 23:15:14 +0100

    101-hbnb.js and en-gb.js files pushed, alongside typo correctoin in 4-hbnb.jsand 100-hbnb.js

--- web_dynamic/static/scripts/101-hbnb.js
+++ web_dynamic/static/scripts/101-hbnb.js
@@ -4,6 +4,36 @@ const checkedCities = {};
 const placeSearchURL = 'http://0.0.0.0:5001/api/v1/places_search/';
 const statusURL = 'http://0.0.0.0:5001/api/v1/status/';
 
+
+function getReviews (place_id) {
+  let reviews;
+  const getReviewsURL = 'http://0.0.0.0:5001/api/v1/places/' + place_id + '/reviews';
+  const getUserURL = 'http://0.0.0.0:5001/api/v1/users/';
+  $.ajax({
+    async: false,
+    url: getReviewsURL,
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      reviews = data;
+    }
+  });
+  for (let i = 0; i < reviews.length; i++) {
+    $.ajax({
+      async: false,
+      url: getUserURL + reviews[i].user_id,
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        console.log(moment(data.updated_at, 'MMMM Do YYYY'));
+        reviews[i].userName = data.first_name + ' ' + data.last_name;
+      }
+    });
+  }
+  console.log(reviews);
+  return reviews;
+}
+
 function placeHtml (place) {
   const guest = (place.max_guest === 1) ? 'Guest' : 'Guests';
   const bedroom = (place.number_rooms === 1) ? 'Bedroom' : 'Bedrooms';
@@ -27,11 +57,22 @@ function placeHtml (place) {
       '</div>'
       : ''
     }
+    <div class="reviews">
+    <h2>Reviews <span>Show</span></h2>
+       	<ul>
+			<li>
+				<h3>From Jordan Peterson the 9th November 2014</h3>
+				<p>I had a great time witsdh family, I highly recommend it four a family gathering.</p>
+			</li>
+		</ul>
+	  </div>
     </article>`;
+  getReviews(place.id);
   return html;
 }
 
 $(document).ready(function () {
+  const moment = moment();
   const sectionPlaces = $('SECTION.places');
   const btn = $('.filters button');
 
--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -10,7 +10,7 @@ function placeHtml (place) {
   const html = `<article>
       <div class="title_box">
         <h2>${place.name}</h2>
-        <div class="price_by_night">${place.price_by_night}</div>
+        <div class="price_by_night">$${place.price_by_night}</div>
       </div>
 
       <div class="information">
